8933d3b27279fd72c449da84cab09408f6ed3a08
JaeKwan Ahn <52008@naver.com>
2023-07-11 20:19:29 +0900

    Test: Header 컴포넌트 import 테스트

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,5 @@
-export default function Header() {
-  return <div className="text-xl font-medium text-black">Im header</div>;
-}
+export const Header = () => {
+  return <div>Im header</div>;
+};
+
+export default Header;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Introduce from '@/app/components/Introduce';
+import Header from '@/app/components/Header';
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Introduce />
     </main>
   );
