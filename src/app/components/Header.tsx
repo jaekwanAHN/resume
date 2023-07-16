16ac8f6f4676735a22cc4c7df6575b229c09710d
JaeKwan Ahn <52008@naver.com>
2023-07-16 18:18:17 +0900

    Feat: Page router로 변환

--- src/app/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-export const Header = () => {
-  return <div>뭐가 문제일까</div>;
-};
-
-export default Header;
--- src/app/components/Project/Project_Title.tsx
+++
@@ -1,5 +0,0 @@
-export const Project = () => {
-  return <div>Test Project</div>;
-};
-
-export default Project;
