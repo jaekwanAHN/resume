16a18c5323860d82f6676a1a25a632097ac472d0
JaeKwan Ahn <52008@naver.com>
2023-07-20 21:55:28 +0900

    Refactor: 정보 파일 따로 분류

---
+++ src/data/personal.ts
@@ -0,0 +1,7 @@
+export const PersonalInformation = {
+  name: '안재관',
+  phoneNumber: '010-8003-3635',
+  eMail: 'ggstork@gmail.com',
+  github: 'https://github.com/jaekwanAHN',
+  blog: 'https://jaekwan.tistory.com/',
+};
