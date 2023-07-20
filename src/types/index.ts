6aedee5d143afd81fd1484a0d084f0f037a17bc4
JaeKwan Ahn <52008@naver.com>
2023-07-20 22:16:33 +0900

    Feat: 프로젝트내용 타입 생성

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export interface ProjectInfo {
+  title: string;
+  description: string;
+  projectDate: string;
+  projectParticipants: string;
+  deployLink: string;
+  githubLink: string;
+  stackList: string[];
+  feature: string[];
+}
