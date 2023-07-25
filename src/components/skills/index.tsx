5b9412b1f9f596ba5493d837be752abd398f7b8b
JaeKwan Ahn <52008@naver.com>
2023-07-25 16:57:12 +0900

    Feat: 주요 기술 강조 표시

--- src/components/skills/index.tsx
+++ src/components/skills/index.tsx
@@ -4,11 +4,11 @@ const Skills = () => {
   return (
     <div className="pb-2">
       <p className="text-2xl text-blue-500">Skills</p>
-      {PersonalInformation.skills[0].join(' ')}
+      <strong>{PersonalInformation.skills[0].join(', ')}</strong>
       <br />
-      {PersonalInformation.skills[1].join(' ')}
+      {PersonalInformation.skills[1].join(', ')}
       <br />
-      {PersonalInformation.skills[2].join(' ')}
+      {PersonalInformation.skills[2].join(', ')}
     </div>
   );
 };
