f4be33812f2522a3db993a420c4f1291b3a53218
JaeKwan Ahn <52008@naver.com>
2023-08-02 22:00:43 +0900

    Refactor: 활동란 list 형태로 수정

--- src/components/activities/indext.tsx
+++ src/components/activities/indext.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { Fragment } from 'react';
 
 interface activitiesProps {
   activities: {
@@ -10,19 +9,19 @@ interface activitiesProps {
 
 const Activities = ({ activities }: activitiesProps) => {
   return (
-    <div className="section">
+    <ul className="section marker:text-blue-500 list-disc list-inside mb-2">
       <p className="text-2xl text-blue-500 sectionTitle">Activities</p>
       {activities.map((activity) => {
         return (
-          <Fragment key={activity.title}>
+          <li key={activity.title}>
             <Link href={activity.link}>
               <i>{activity.title}</i>
             </Link>
             <br />
-          </Fragment>
+          </li>
         );
       })}
-    </div>
+    </ul>
   );
 };
 
