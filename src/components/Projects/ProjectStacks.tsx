a2768345f57626ed6b069c775021035f8dccf63f
JaeKwan Ahn <52008@naver.com>
2023-08-02 22:01:27 +0900

    Feat: 프로젝트 기술 스택 표기 변경

--- src/components/Projects/ProjectStacks.tsx
+++ src/components/Projects/ProjectStacks.tsx
@@ -4,9 +4,18 @@ interface ProjectStacks {
 
 const ProjectStacks = ({ stackList }: ProjectStacks) => {
   return (
-    <>
-      <p>기술 스택: {stackList.join(', ')}</p>
-    </>
+    <ul className="mb-2">
+      {stackList.map((stack) => {
+        return (
+          <li
+            key={stack}
+            className="inline-block bg-blue-500 rounded text-white mr-1 px-1"
+          >
+            {stack}
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
